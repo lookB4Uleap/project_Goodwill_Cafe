f9d7a0c0c29e2da9da0cab5e5b9b1f53fb59119c
Anish Sarkar <anish.sarkar.official@gmail.com>
2021-12-04 17:44:16 +0530

    add environmental variables

--- util/sqlconfig.js
+++ util/sqlconfig.js
@@ -1,8 +1,8 @@
 const sqlconfig = {
-    user: 'anish',
-    password: 'retshift123-AS',
-    database: 'goodwill-db',
-    server: 'goodwill-server.database.windows.net',
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE,
+    server: process.env.SERVER,
     options: {
       encrypt: true, // for azure
       trustServerCertificate: false // change to true for local dev / self-signed certs
